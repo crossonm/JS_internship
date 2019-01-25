48392597a659b3fc5da9cc9e710562134214d44f
crossonm <maryrcrosson@gmail.com>
2019-01-25 15:00:06 -0600

    n1

---
+++ prog1_dateTime.js
@@ -0,0 +1,15 @@
+//M.S. Crosson    //prog1.js
+//25 JAN 2019     //JSIntern
+
+/* Task is to create a program to display current date/time
+in the format: 
+'Today is : Tuesday.
+Current time is: 10 PM:30:38'                             */
+
+now = new Date()
+console.log(now)
+
+
+
+
+
---
+++ template.js
@@ -0,0 +1,5 @@
+//M.S. Crosson    //prog1.js
+//25 JAN 2019     //JSIntern
+
+/* Task is to....   */
+
